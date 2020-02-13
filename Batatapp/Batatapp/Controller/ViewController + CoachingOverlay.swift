09473a01372731997869e7de8577906d52d37f75
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-02-13 17:00:59 -0300

    Adicionando coach (#5)

---
+++ Batatapp/Batatapp/Controller/ViewController + CoachingOverlay.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController + CoachingOverlay.swift
+//  Batatapp
+//
+//  Created by Pedro Giuliano Farina on 13/02/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+extension ViewController: ARCoachingOverlayViewDelegate {
+    func coachingOverlayViewWillActivate(_ coachingOverlayView: ARCoachingOverlayView) {
+        //Esconder tudo
+    }
+
+    func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
+        //Mostrar tudo
+    }
+
+    func coachingOverlayViewDidRequestSessionReset(_ coachingOverlayView: ARCoachingOverlayView) {
+        //Resetar tracking
+    }
+
+    func setupCoachingOverlay() {
+        coachingOverlay.delegate = self
+        coachingOverlay.session = session
+        coachingOverlay.goal = .tracking
+
+        coachingOverlay.translatesAutoresizingMaskIntoConstraints = false
+        arView.addSubview(coachingOverlay)
+
+        NSLayoutConstraint.activate([
+            coachingOverlay.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            coachingOverlay.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            coachingOverlay.widthAnchor.constraint(equalTo: view.widthAnchor),
+            coachingOverlay.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+    }
+}
