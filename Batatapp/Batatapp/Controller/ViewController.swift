f29579943f347f47ded9727da5e87169e0100616
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-02-13 17:43:13 -0300

    Adicionando multi peer (#7)
    * Adicionando coach
    
    * Adicionando multipeer com caixas pra verificação
    
    * Retirando caixas

--- Batatapp/Batatapp/Controller/ViewController.swift
+++ Batatapp/Batatapp/Controller/ViewController.swift
@@ -9,8 +9,36 @@
 import UIKit
 import ARKit
 import RealityKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate, MultipeerHandler {
+
+    lazy var multipeerManager: MultipeerManager = MultipeerManager(serviceType: "potatoBomb", handler: self)
+
+    func peerDiscovered(_ id: MCPeerID) -> Bool {
+        return true
+    }
+
+    func peerReceivedInvitation(_ id: MCPeerID) -> Bool {
+        return true
+    }
+
+    func session(_ session: ARSession, didOutputCollaborationData data: ARSession.CollaborationData) {
+        if !multipeerManager.connectedPeers.isEmpty {
+            guard let encodedData = try? NSKeyedArchiver.archivedData(withRootObject: data, requiringSecureCoding: true)
+                else { fatalError("Unexpectedly failed to encode collaboration data.") }
+            let dataIsCritical = data.priority == .critical
+            multipeerManager.sendToAllPeers(encodedData, reliably: dataIsCritical)
+        }
+    }
+
+    func receivedData(_ data: Data, from peerID: MCPeerID) {
+        if let collaborationData = try? NSKeyedUnarchiver.unarchivedObject(ofClass: ARSession.CollaborationData.self, from: data) {
+            session.update(with: collaborationData)
+            return
+        }
+    }
+
     
     @IBOutlet var arView: ARView!
     var scene: Scene {
@@ -24,6 +52,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        _ = multipeerManager
         setupCoachingOverlay()
+
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.isCollaborationEnabled = true
+        configuration.environmentTexturing = .automatic
+        session.run(configuration)
+        session.delegate = self
+
+        UIApplication.shared.isIdleTimerDisabled = true
+
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        arView.addGestureRecognizer(gesture)
+    }
+
+    @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
+
+    }
+
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        
     }
 }
