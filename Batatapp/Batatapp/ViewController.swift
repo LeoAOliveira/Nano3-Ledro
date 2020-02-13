a35b79d97095d6ff642da43c3ad84d0b0ab18502
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-02-13 15:31:54 -0300

    Projeto principal (#1)
    * Criando projeto
    
    * Limpando projeto

---
+++ Batatapp/Batatapp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Batatapp
+//
+//  Created by Pedro Giuliano Farina on 13/02/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    }
+}
