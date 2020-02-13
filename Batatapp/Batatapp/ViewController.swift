d9b64d5acaa9e8558a87ef0409adc1cbcd1511b0
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-02-13 16:35:39 -0300

    Mudando pra Reality Kit

--- Batatapp/Batatapp/ViewController.swift
+++ Batatapp/Batatapp/ViewController.swift
@@ -7,31 +7,20 @@
 //
 
 import UIKit
-import SceneKit
 import ARKit
+import RealityKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    private var scene: Scene {
+        return arView.scene
+    }
+    private var session: ARSession {
+        return arView.session
+    }
     
     override func viewDidLoad() {
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        super.viewDidLoad()
     }
 }
